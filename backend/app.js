3e076fe68ad32b3c27aba30b2e2d2569f0bcb21d
Roman Ismagilov <roman.j123@ya.ru>
2021-06-30 00:28:32 +0300

    fix: cors

--- backend/app.js
+++ backend/app.js
@@ -24,7 +24,7 @@ app.use(bodyParser.json());
 
 // Add Access Control Allow Origin headers
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "https://yoursite.com");
+  res.setHeader("Access-Control-Allow-Origin", "http://roman-j123.nomoredomains.monster/");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
